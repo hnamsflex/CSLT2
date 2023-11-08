4dcfc4b9ac08866391c15b2d6dc957df374c886e
hnamsflex <hnamsflex@gmail.com>
2023-11-08 23:15:34 +0700

    add exercise 34

---
+++ exercise34.py
@@ -0,0 +1,8 @@
+# Read an integer from the user
+number = int(input("Enter an integer: "))
+
+# Check if the number is even or odd
+if number % 2 == 0:
+    print(number,"is even.")
+else:
+    print(number,"is odd.")
